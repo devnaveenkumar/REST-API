9bcac33dc254c4281f8ce35b87d946cc7e8d9e80
devnaveenkumar <98394930+devnaveenkumar@users.noreply.github.com>
2023-08-30 11:24:44 +0530

    Add files via upload

---
+++ Day-8-Exercise/src/main/java/com/example/demo/Day8ExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day8ExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day8ExerciseApplication.class, args);
+	}
+
+}
